52b83494be58fe1336bb36101e79b704031f59fa
htke1 <sheraptenzin98@gmail.com>
2021-08-02 09:46:58 +0530

    minor patch

--- src/postList.js
+++ src/postList.js
@@ -14,9 +14,7 @@ import {
 export const PostList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
-            <ReferenceField source="userId" reference="users">
-                <TextField source="name" />
-            </ReferenceField>
+            
             <TextField source="id" />
             <TextField source="title" />
             <TextField source="body" />
