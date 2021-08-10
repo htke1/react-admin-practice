3dbe38ae239f9c1442d24f567ed2c93421a675e0
htke1 <sheraptenzin98@gmail.com>
2021-08-10 09:50:27 +0530

    Show and Edit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React from 'react';
 import {Admin, Resource} from 'react-admin'
 import jsonServerProvider from 'ra-data-json-server';
 import { UserList } from './userList';
-import { PostList, PostCreate } from './postList';
+import { PostList, PostCreate, PostShow, PostEdit } from './postList';
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
 function App(){
   return (
     <Admin dataProvider={dataProvider}>
-      <Resource name="posts" list={PostList}  create={PostCreate}/>
+      <Resource name="posts"  list={PostList} show={PostShow} edit={PostEdit} create={PostCreate}/>
            <Resource name="users" list={UserList} />
    </Admin>
   );
--- src/postList.js
+++ src/postList.js
@@ -9,12 +9,16 @@ import {
     ReferenceInput,
     SelectInput,
     TextInput,
+    Show, Edit, SimpleShowLayout, DateField, RichTextField,EditButton,ReferenceManyField, RichTextInput,DateInput, required
 } from 'react-admin';
+
 
 export const PostList = props => (
     <List {...props}>
         <Datagrid rowClick="edit">
-            
+            <ReferenceField source="userId" reference="users">
+                <TextField source="name" />
+            </ReferenceField>
             <TextField source="id" />
             <TextField source="title" />
             <TextField source="body" />
@@ -25,11 +29,39 @@ export const PostList = props => (
 export const PostCreate = props => (
         <Create {...props}>
             <SimpleForm>
-                <ReferenceInput source="userId" reference="users">
-                    <SelectInput optionText="name" />
-                </ReferenceInput>
+            <ReferenceInput  source="userId" reference="users">
+            <SelectInput optionText="name" />
+        </ReferenceInput>
                 <TextInput source="title" />
                 <TextInput multiline source="body" />
             </SimpleForm>
         </Create>
-    );
+    );  
+
+export const PostShow = (props) => (
+    <Show {...props}>
+        <SimpleShowLayout>
+            <TextField source="title" />
+            <RichTextField source="body" />
+        </SimpleShowLayout>
+    </Show>
+); 
+
+
+export const PostEdit = (props) => (
+    <Edit {...props}>
+        <SimpleForm>
+            <TextInput disabled label="Id" source="id" />
+            <TextInput source="title" validate={required()} />
+            <TextInput multiline source="teaser" validate={required()} />
+            <TextInput source="body" validate={required()} />
+            <ReferenceManyField  label="Comments" reference="posts" target="post_id">
+                <Datagrid>
+                    <TextField source="body" />
+                    <DateField source="created_at" />
+                    <EditButton />
+                </Datagrid>
+            </ReferenceManyField>
+        </SimpleForm>
+    </Edit>
+);
